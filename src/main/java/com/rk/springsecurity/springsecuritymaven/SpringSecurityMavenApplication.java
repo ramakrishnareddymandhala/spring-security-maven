9604ca54f33cba5ed18d7321c36f8a7c9979d9aa
Ramakrishna.Mandhala <Ramakrishna.Mandhala@LP-J2YZ4Q2.GEO.CORP.HCL.IN>
2019-02-10 13:30:31 +0100

    Adding spring security project

---
+++ src/main/java/com/rk/springsecurity/springsecuritymaven/SpringSecurityMavenApplication.java
@@ -0,0 +1,14 @@
+package com.rk.springsecurity.springsecuritymaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMavenApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/rk/springsecurity/springsecuritymaven/SpringSecurityMavenApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rk.springsecurity.springsecuritymaven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSecurityMavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
